64853faf10b5574a46cca630a97f69f0a2f8c890
Maksym Rakomin <maksym.rakomin@gmail.com>
2021-12-05 09:06:23 +0200

    first commit

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,13 @@
+import './app-info.css'
+
+const AppInfo = () => {
+  return (
+      <div className='app-info'>
+          <h1>Учет сотрудников в компании N</h1>
+          <h2>Общее чило сотрудников: 4</h2>
+          <h2>Премию получат: 3</h2>
+      </div>
+  )
+}
+
+export default AppInfo
---
+++ src/components/search-panel/search-panel.js
@@ -0,0 +1,9 @@
+import './search-panel.css'
+
+const SearchPanel = () => {
+  return (
+      <input type="text" className="form-control search-input" placeholder="Найли сотрудника"/>
+  )
+}
+
+export default SearchPanel
